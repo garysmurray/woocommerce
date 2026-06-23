9ad7e3af70906fa413a34bb11dacff2eafc39f0a
kalessil <kalessil@gmail.com>
2026-06-23 19:12:35 +0200

    Performance: persistent counters infrastructure components.

--- plugins/woocommerce/src/Utilities/ProductUtil.php
+++ plugins/woocommerce/src/Utilities/ProductUtil.php
@@ -7,6 +7,8 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\Utilities;
 
+use Automattic\WooCommerce\Caches\ProductCountCache;
+
 /**
  * A class of utilities related to products.
  */
@@ -20,8 +22,13 @@ final class ProductUtil {
 	 * @return array<string,int>
 	 */
 	public static function get_count_for_type( string $product_type ): array {
-		// Performance note: integration point for upcoming persistent counters solution.
-		$count_per_status = (array) wp_count_posts( $product_type );
+		$product_count_cache = new ProductCountCache();
+		$count_per_status    = $product_count_cache->get( $product_type );
+
+		if ( null === $count_per_status ) {
+			$count_per_status = (array) wp_count_posts( $product_type );
+			$product_count_cache->set_multiple( $product_type, $count_per_status );
+		}
 
 		return array_map( 'intval', $count_per_status );
 	}
